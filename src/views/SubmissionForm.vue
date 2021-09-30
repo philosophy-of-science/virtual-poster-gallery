6b1f502e8d9d385e0288c0192ac27521b67de0b3
Ryan Feigenbaum <director@philsci.org>
2021-09-29 22:25:01 -0400

    implemented editor

--- src/views/SubmissionForm.vue
+++ src/views/SubmissionForm.vue
@@ -10,13 +10,17 @@
         <input type="text" id="title" v-model="title" required />
         <label for="authors">Author(s)</label>
         <input type="text" id="authors" v-model="authors" required />
+        <label for="abstract">abstract</label>
         <quill-editor
+          class="editor"
           ref="myQuillEditor"
           v-model="content"
           @blur="onEditorBlur($event)"
           @focus="onEditorFocus($event)"
           @ready="onEditorReady($event)"
+          :options="editorOption"
         />
+        <div v-html="sanitizedAbstract"></div>
         <label for="abstract">abstract</label>
         <textarea id="abstract" v-model="abstract" required />
         <label for="topic">Topic</label>
@@ -91,7 +95,7 @@
 import { mapState, mapActions } from 'vuex';
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
-import 'quill/dist/quill.bubble.css';
+import sanitize from 'sanitize-html';
 import { quillEditor } from 'vue-quill-editor';
 import slugify from 'slugify';
 import Card from '@/components/Card.vue';
@@ -119,6 +123,22 @@ export default {
       loading: false,
       loadingImg: false,
       content: '',
+      editorOption: {
+        modules: {
+          toolbar: [
+            'bold',
+            'italic',
+            'link',
+            'blockquote',
+            { list: 'bullet' },
+            { list: 'ordered' },
+            { script: 'sub' },
+            { script: 'super' },
+            'code-block',
+            'clean',
+          ],
+        },
+      },
     };
   },
 
@@ -126,6 +146,12 @@ export default {
     ...mapState(['user', 'poster']),
     editor() {
       return this.$refs.myQuillEditor.quill;
+    },
+    sanitizedAbstract() {
+      if (this.content) {
+        return sanitize(this.content);
+      }
+      return null;
     },
   },
 
@@ -261,11 +287,12 @@ export default {
       return topicMap[topic] && topicMap[topic].slug;
     },
 
-    onEditorBlur(quill) {
-      console.log('editor blur!', quill);
+    onEditorBlur() {
+      this.$refs.myQuillEditor.$el.classList.remove('focus');
     },
-    onEditorFocus(quill) {
-      console.log('editor focus!', quill);
+    onEditorFocus() {
+      console.log(this.$refs);
+      this.$refs.myQuillEditor.$el.classList.add('focus');
     },
     onEditorReady(quill) {
       console.log('editor ready!', quill);
@@ -281,6 +308,7 @@ export default {
       this.title = this.poster.title;
       this.authors = this.poster.authors;
       this.abstract = this.poster.abstract;
+      this.content = this.poster.abstract;
       this.selected = this.poster.topic;
       this.topic_slug = this.poster.topic_slug;
       this.image = this.poster.image;
@@ -314,7 +342,8 @@ label,
 
 input,
 textarea,
-select {
+select,
+.editor {
   width: 100%;
   padding: 0.5rem 0.75rem;
   font-family: inherit;
@@ -325,7 +354,8 @@ select {
   border-radius: var(--radius);
   transition: border 0.2s, background 0.2s;
 
-  &:focus {
+  &:focus,
+  &.focus {
     background: var(--lighter);
     border: 2px solid var(--teal);
     outline: none;
@@ -415,5 +445,112 @@ textarea {
   ul {
     padding-left: 2rem;
   }
+}
+
+.ql-toolbar.ql-snow {
+  padding: 0 !important;
+  border: none !important;
+}
+.ql-container.ql-snow {
+  height: 40ch;
+  padding: 0 !important;
+  border: 0 !important;
+  border-top: 1px solid var(--dark) !important;
+}
+.quill-editor,
+.ql-container {
+  font-family: inherit !important;
+}
+.ql-editor {
+  padding: 0.5rem 0 0 !important;
+  font-family: inherit !important;
+  font-size: 0.9rem !important;
+}
+
+.ql-snow a {
+  color: var(--dark) !important;
+}
+
+.ql-snow.ql-toolbar button:hover .ql-stroke,
+.ql-snow .ql-toolbar button:hover .ql-stroke,
+.ql-snow.ql-toolbar button:focus .ql-stroke,
+.ql-snow .ql-toolbar button:focus .ql-stroke,
+.ql-snow.ql-toolbar button.ql-active .ql-stroke,
+.ql-snow .ql-toolbar button.ql-active .ql-stroke,
+.ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke,
+.ql-snow .ql-toolbar .ql-picker-label:hover .ql-stroke,
+.ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-stroke,
+.ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-stroke,
+.ql-snow.ql-toolbar .ql-picker-item:hover .ql-stroke,
+.ql-snow .ql-toolbar .ql-picker-item:hover .ql-stroke,
+.ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-stroke,
+.ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-stroke,
+.ql-snow.ql-toolbar button:hover .ql-stroke-miter,
+.ql-snow .ql-toolbar button:hover .ql-stroke-miter,
+.ql-snow.ql-toolbar button:focus .ql-stroke-miter,
+.ql-snow .ql-toolbar button:focus .ql-stroke-miter,
+.ql-snow.ql-toolbar button.ql-active .ql-stroke-miter,
+.ql-snow .ql-toolbar button.ql-active .ql-stroke-miter,
+.ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke-miter,
+.ql-snow .ql-toolbar .ql-picker-label:hover .ql-stroke-miter,
+.ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-stroke-miter,
+.ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-stroke-miter,
+.ql-snow.ql-toolbar .ql-picker-item:hover .ql-stroke-miter,
+.ql-snow .ql-toolbar .ql-picker-item:hover .ql-stroke-miter,
+.ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-stroke-miter,
+.ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-stroke-miter {
+  stroke: var(--teal) !important;
+}
+
+.ql-editor.ql-blank::before {
+  left: 0 !important;
+}
+
+.ql-snow.ql-toolbar button:hover,
+.ql-snow .ql-toolbar button:hover,
+.ql-snow.ql-toolbar button:focus,
+.ql-snow .ql-toolbar button:focus,
+.ql-snow.ql-toolbar button.ql-active,
+.ql-snow .ql-toolbar button.ql-active,
+.ql-snow.ql-toolbar .ql-picker-label:hover,
+.ql-snow .ql-toolbar .ql-picker-label:hover,
+.ql-snow.ql-toolbar .ql-picker-label.ql-active,
+.ql-snow .ql-toolbar .ql-picker-label.ql-active,
+.ql-snow.ql-toolbar .ql-picker-item:hover,
+.ql-snow .ql-toolbar .ql-picker-item:hover,
+.ql-snow.ql-toolbar .ql-picker-item.ql-selected,
+.ql-snow .ql-toolbar .ql-picker-item.ql-selected {
+  color: var(--teal);
+}
+
+.ql-snow.ql-toolbar button:hover .ql-fill,
+.ql-snow .ql-toolbar button:hover .ql-fill,
+.ql-snow.ql-toolbar button:focus .ql-fill,
+.ql-snow .ql-toolbar button:focus .ql-fill,
+.ql-snow.ql-toolbar button.ql-active .ql-fill,
+.ql-snow .ql-toolbar button.ql-active .ql-fill,
+.ql-snow.ql-toolbar .ql-picker-label:hover .ql-fill,
+.ql-snow .ql-toolbar .ql-picker-label:hover .ql-fill,
+.ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-fill,
+.ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-fill,
+.ql-snow.ql-toolbar .ql-picker-item:hover .ql-fill,
+.ql-snow .ql-toolbar .ql-picker-item:hover .ql-fill,
+.ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-fill,
+.ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-fill,
+.ql-snow.ql-toolbar button:hover .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar button:hover .ql-stroke.ql-fill,
+.ql-snow.ql-toolbar button:focus .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar button:focus .ql-stroke.ql-fill,
+.ql-snow.ql-toolbar button.ql-active .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar button.ql-active .ql-stroke.ql-fill,
+.ql-snow.ql-toolbar .ql-picker-label:hover .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar .ql-picker-label:hover .ql-stroke.ql-fill,
+.ql-snow.ql-toolbar .ql-picker-label.ql-active .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar .ql-picker-label.ql-active .ql-stroke.ql-fill,
+.ql-snow.ql-toolbar .ql-picker-item:hover .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar .ql-picker-item:hover .ql-stroke.ql-fill,
+.ql-snow.ql-toolbar .ql-picker-item.ql-selected .ql-stroke.ql-fill,
+.ql-snow .ql-toolbar .ql-picker-item.ql-selected .ql-stroke.ql-fill {
+  fill: var(--teal);
 }
 </style>
